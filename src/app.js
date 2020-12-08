0c5f4e04dfd442d04611ece54cf2b9ff2063ba8f
Varsharani Ambi <68917117+varsha-ambi@users.noreply.github.com>
2020-12-08 23:29:06 +0530

    s s

--- src/app.js
+++ src/app.js
@@ -1,28 +1,27 @@
-
-const express = require('express');
-const app = express()
-
+const express = require("express");
+const app = express();
+const Subscriber = require("./models/subscribers");
+const ObjectId = require("mongoose");
 
 // Your code goes here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/subscribers", (req, res) => {
+  Subscriber.find().then((subscribers) => res.send(subscribers));
+  return;
+});
+app.get("/subscribers/names", (req, res) => {
+  Subscriber.find()
+    .select({ name: 1, subscribedChannel: 1 })
+    .then((subscribers) => res.send(subscribers));
+  return;
+});
+app.get("/subscribers/:id", (req, res) => {
+  const id = req.params.id;
+  Subscriber.find({ _id: id })
+    .then((subscribers) =>
+      subscribers.map((subscribers) => res.send(subscribers))
+    )
+    .catch((error) => res.status(400).send({ message: error.message }));
+  return;
+});
 
 module.exports = app;
